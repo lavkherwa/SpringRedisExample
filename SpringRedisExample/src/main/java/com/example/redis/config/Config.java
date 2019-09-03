e7036a4522f8649f589a3317f9dda8e4ada85162
apple <apple@192.168.0.104>
2019-09-03 22:56:26 +0530

    update - defining custom host and port for redis server in the configuration

--- SpringRedisExample/src/main/java/com/example/redis/config/Config.java
+++ SpringRedisExample/src/main/java/com/example/redis/config/Config.java
@@ -1,8 +1,13 @@
 package com.example.redis.config;
 
+import java.time.Duration;
+
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration.JedisClientConfigurationBuilder;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -12,15 +17,42 @@ import com.example.redis.model.User;
 @EnableCaching
 public class Config {
 
+	/*- BELOW CODE IS DEPRECATED FOR SPRING 2.X
+		@Bean
+		public JedisConnectionFactory JedisConnectionFactory() {
+			JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
+			jedisConFactory.setHostName("localhost");
+			jedisConFactory.setPort(6379);
+			return jedisConFactory;
+		}
+	*/
+
 	@Bean
-	public JedisConnectionFactory JedisConnectionFactory() {
-		return new JedisConnectionFactory();
+	JedisConnectionFactory jedisConnectionFactory() {
+
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		/*
+		 * By default spring configure it for local but here is how you can provide
+		 * custom connection properties
+		 */
+		redisStandaloneConfiguration.setHostName("localhost");
+		redisStandaloneConfiguration.setPort(6379);
+//		redisStandaloneConfiguration.setDatabase(0);
+//		redisStandaloneConfiguration.setPassword(RedisPassword.of("password"));
+
+		JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+		jedisClientConfiguration.connectTimeout(Duration.ofSeconds(60));// 60s connection timeout
+
+		JedisConnectionFactory jedisConFactory = new JedisConnectionFactory(redisStandaloneConfiguration,
+				jedisClientConfiguration.build());
+
+		return jedisConFactory;
 	}
 
 	@Bean
 	public RedisTemplate<String, User> redisTemplate() {
 		final RedisTemplate<String, User> template = new RedisTemplate<>();
-		template.setConnectionFactory(JedisConnectionFactory());
+		template.setConnectionFactory(jedisConnectionFactory());
 		return template;
 	}
 
